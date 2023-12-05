e0173ab50677dadb1f2da7932d6047768f4de343
romantkacik <123880795+romantkacik@users.noreply.github.com>
2023-12-05 13:16:54 +0100

    first commit

---
+++ vite/main.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+const COLOR_PALETTE = {
+  '#28262C': 'Raisin Black',
+  '#998FC7': 'Blue Bell',
+  '#D4C2FC': 'Lavender Blue',
+  '#F9F5FF': 'Magnolia',
+  '#14248A': 'Resolution Blue',
+}
