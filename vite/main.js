6c712122ff22f8748e005d65f8c7434f6d6bc005
romantkacik <123880795+romantkacik@users.noreply.github.com>
2023-12-05 19:13:08 +0100

    problemas con js

--- vite/main.js
+++ vite/main.js
@@ -7,3 +7,40 @@ const COLOR_PALETTE = {
   '#F9F5FF': 'Magnolia',
   '#14248A': 'Resolution Blue',
 }
+
+Object.keys(COLOR_PALETTE).forEach((color) => {
+  const option = document.createElement('option')
+  option.value = color
+  option.innerText = COLOR_PALETTE[color]
+})
+
+Object.keys(COLOR_PALETTE).forEach((color) => {
+  const option = document.createElement('option')
+  option.value = color
+  option.innerText = COLOR_PALETTE[color]
+
+  colorPickerSelect.append(option)
+})
+const addOptionsToColorPicker = () => {
+  const colorPickerSelect = document.querySelector('#color-picker')
+}
+
+addOptionsToColorPicker()
+
+const addEventListenerToColorPicker = () => {
+  const colorPickerSelect = document.querySelector('#color-picker')
+
+  colorPickerSelect.addEventListener('change', (event) => {
+    const newColor = event.target.value
+    //Almacenamos el código de color
+
+    document.body.style.backgroundColor = newColor
+    //Le aplicamos el background color con el código de color seleccionado
+  })
+}
+
+addOptionsToColorPicker()
+addEventListenerToColorPicker()
+
+const colorNameText = `${COLOR_PALETTE[newColor]} | ${newColor}`
+colorName.innerText = COLOR_PALETTE[newColor] ? colorNameText : '-'
